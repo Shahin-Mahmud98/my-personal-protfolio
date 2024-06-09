f5ea96ed337b56a0a45a6758e3801a99b5c993e4
Shahin-Mahmud98 <smshahinmahmud6@gmail.com>
2024-06-09 17:30:45 +0600

    added file

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,9 +3,15 @@ import bannerImg from "./images/bannerImg.png";
 import img from "./images/img.jpg";
 import img2 from "./images/img2.jpg";
 import img3 from "./images/img3.jpg";
+// import Exetic from "./images/Exetic.PNG"
+// import Ema from "./images/Ema.PNG"
+// import Property from "./images/Property.PNG"
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpeg";
+import Exetic from "./images/projects/Exetic.jpg";
+import Ema from "./images/projects/Ema.jpg";
+import Property from "./images/projects/Property.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
@@ -14,6 +20,9 @@ import contactImg from "./images/contact/contactImg.png";
 
 export {
   profile,
+  Exetic,
+  Ema,
+  Property,
   img,
   img2,
   img3,
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,5 @@
 import Title from "../../Layout/Title";
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import {  Property, Ema, Exetic } from "../../assets/index";
 import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
@@ -15,40 +15,40 @@ const Projects = () => {
            </div>
            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
+          title="Builders Web App"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
+          src={Property}
         />
         <ProjectsCard
           title="E-commerce Website"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
+          src={Ema}
+        />
+        <ProjectsCard
+          title="E-commerce Website Car"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={Exetic}
+        />
+        <ProjectsCard
+          title="Builders Web App"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={Property}
+        />
+        <ProjectsCard
+          title="E-commerce Website Car"
+          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          src={Exetic}
         />
         <ProjectsCard
           title="Chatting App"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectThree}
-        />
-        <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectThree}
-        />
-        <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
-        />
-        <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
+          src={Ema}
         />
       </div>
         </section>
